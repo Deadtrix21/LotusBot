b32e65deb4089efcf476bc4da3bb8709f6c8a275
Deadtrix21 <NightMareDeadTrix@outlook.com>
2023-11-05 17:32:17 +0200

    Updates

--- src/extensions/ApexLegends/cog.py
+++ src/extensions/ApexLegends/cog.py
@@ -71,33 +71,33 @@ class ApexLegends(Extension):
                 await ctx.respond(embed=i)
             return
 
-    @bridge_command(name='apex-legend-stats')
-    @discord.option(name="platform", choices=PLATFORM, description="Search platform")
-    @discord.option(name="legend", choices=LEGENDS, description="Searched player legend")
-    @discord.option(name="player", description="Search player name")
-    async def apex_legend_stats(self, ctx: BridgeContext, platform: str = None, legend: str = None, player: str = None):
-        """
-        Get a players stats on a legend
-        Please make sure to use Upper Case
-
-        platform: Example: Xbox
-        legend: Example: Octane
-        player: Example: DeadTrix4053
-        """
-        if platform == None or legend == None or player == None:
-            return await ctx.reply("You missed some details.")
-        platform = "PC" if platform == "PC" else platform
-        platform = "PS4" if platform == "Playstation" else platform
-        platform = "X1" if platform == "Xbox" else platform
-        await ctx.reply("Starting Search")
-        data = json.loads(urllib3.request("get",
-                                          "https://lil2-gateway.apexlegendsstatus.com/gateway.php?qt=stats-single-legend&userName="
-                                          + player + "&userPlatform=" + platform + "&userLegend=" + legend).data)
-        with open("file.json", "w") as file:json.dump(data, file)
-        if isinstance(ctx, BridgeExtContext):
-            pass
-        elif isinstance(ctx, BridgeApplicationContext):
-            pass
+    # @bridge_command(name='apex-legend-stats')
+    # @discord.option(name="platform", choices=PLATFORM, description="Search platform")
+    # @discord.option(name="legend", choices=LEGENDS, description="Searched player legend")
+    # @discord.option(name="player", description="Search player name")
+    # async def apex_legend_stats(self, ctx: BridgeContext, platform: str = None, legend: str = None, player: str = None):
+    #     """
+    #     Get a players stats on a legend
+    #     Please make sure to use Upper Case
+    #
+    #     platform: Example: Xbox
+    #     legend: Example: Octane
+    #     player: Example: DeadTrix4053
+    #     """
+    #     if platform == None or legend == None or player == None:
+    #         return await ctx.reply("You missed some details.")
+    #     platform = "PC" if platform == "PC" else platform
+    #     platform = "PS4" if platform == "Playstation" else platform
+    #     platform = "X1" if platform == "Xbox" else platform
+    #     await ctx.reply("Starting Search")
+    #     data = json.loads(urllib3.request("get",
+    #                                       "https://lil2-gateway.apexlegendsstatus.com/gateway.php?qt=stats-single-legend&userName="
+    #                                       + player + "&userPlatform=" + platform + "&userLegend=" + legend).data)
+    #     with open("file.json", "w") as file:json.dump(data, file)
+    #     if isinstance(ctx, BridgeExtContext):
+    #         pass
+    #     elif isinstance(ctx, BridgeApplicationContext):
+    #         pass
 
 
 def setup(bot):
